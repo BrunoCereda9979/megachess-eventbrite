ffa37e4d2818d4c8e389bd9f69508128cc7803ee
BrunoCereda9979 <bruno9979@gmail.com>
2020-12-08 16:01:39 -0300

    Created the Rook class piece

---
+++ src/pieces/Rook.js
@@ -0,0 +1,28 @@
+const Piece = require("../Piece");
+
+/**
+ *  This is the Bishop class, it inherits from the Piece class.
+ */
+
+class Rook extends Piece {
+    constructor (...args) {
+        super (...args);
+    }
+
+   /**
+    *   A Rook can move vertically or horizontally any number of spots it wants.
+    */
+    canMove (fromX, fromY, toX, toY) {
+        super.canMove(fromX, fromY, toX, toY);
+        
+        let canMove = false;
+
+        if ( fromX == toX || fromY == toY ) { 
+            canMove = true;
+        }
+
+        return canMove;
+    } 
+}
+
+module.exports = Rook;
